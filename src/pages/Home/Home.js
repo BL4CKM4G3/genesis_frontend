18f5d8e8806732c6dedbf36e741ca49944c4d3ea
BL4CKM4G3 <alvaro_rangel@outlook.com>
2021-02-10 11:47:31 -0300

    feat: :sparkles: Created home layout

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Button from '../../components/atoms/button/button'
+
+const Home = () => {
+  return (
+    <div>
+      <Button text={"API"}></Button>
+    </div>
+  )
+}
+
+export default Home
